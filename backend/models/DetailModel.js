52752dcbe83252746c6c165269e03c708e5632d1
Bayuaditya24 <bayuaditya115@gmail.com>
2024-08-11 14:32:58 +0800

    bayu punya

---
+++ backend/models/DetailModel.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Detail = db.define(
+  "detail",
+  {
+    perawatanPelanggan: DataTypes.STRING,
+    hargaP: DataTypes.STRING,
+    quantityP: DataTypes.STRING,
+    totalHarga: DataTypes.STRING,
+    grandtotal: DataTypes.STRING,
+    metodeDet: DataTypes.INTEGER,
+    idpenjualan: DataTypes.INTEGER,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default Detail;
+
+(async () => {
+  await db.sync();
+})();
